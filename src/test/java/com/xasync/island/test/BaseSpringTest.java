fc88ff72e42b5227c90dd8f9c2250c19c7164490
xasync <xasync@aliyun.com>
2022-05-08 10:28:58 +0800

    test(spring): some tests for SpringContexts

---
+++ src/test/java/com/xasync/island/test/BaseSpringTest.java
@@ -0,0 +1,14 @@
+package com.xasync.island.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {SpringBootApplicationTest.class})
+public class BaseSpringTest {
+}
